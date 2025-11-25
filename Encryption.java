d2e408adf96657fe46240c5c16370e1d513ddf58
Yoavpan <Yoavpan11@gmail.com>
2025-11-25 15:21:39 +0200

    Add Scanner for user input in Encryption program

--- Encryption.java
+++ Encryption.java
@@ -7,9 +7,11 @@
 public class Encryption
 {
     public static void main(String[] args){
-        
+        Scanner reader = new Scanner(System.in);
         System.out.println("Welcome to the Encryption / Decryption Program");
-        
+        int a = reader.nextInt();
+        int b = reader.nextInt();
+        add(a,b);
     }
     public static int add (int a, int b){
         return a+b;
