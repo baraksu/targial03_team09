e3380dbfc957cfc94a8aa35408d6e03ae861bd36
Yoavpan <yoavpan11@gmail.com>
2025-11-25 17:56:20 +0200

    תוכנה לבדיקת המספר שהוכנס

--- Encryption.java
+++ Encryption.java
@@ -12,8 +12,9 @@ public class Encryption
         System.out.println("Enter 1 for Encrypt | 2 for Decrypt");
 
         int programNum = reader.nextInt();
+        if (checkNum(programNum)) return;
 
-        System.out.println("Enter")
+        System.out.println("Enter up to 3 words sentence");
     }
     //אוהד חסון + קצת תיקונים(יואב פנסקי)
     public static int wordsNum(String string){
@@ -25,5 +26,13 @@ public class Encryption
             return 2;
         else
             return 3;
+    }
+    //תוכנה לבדיקה האם המספר שהוכנס הוא 1 או 2
+    //ע"י יואב פנסקי
+    public static boolean checkNum(int num) {
+        if (num != 1 && num != 2) {
+            System.out.println(num + "is not a valid choice");
+            return true;
+        } else return false;
     }
 }
