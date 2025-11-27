55932193d669cf64b3ab707450de871cb389d438
i5am5cool <ohad.hasson2010@gmail.com>
2025-11-27 22:00:15 +0200

    תיקון פונקציית הצפנה

--- Encryption.java
+++ Encryption.java
@@ -1,7 +1,7 @@
 /**
  * Encryption & Decryption Program
  *
- * @author Yoav Panski (219338951)and Ohad Hasson
+ * @author Yoav Panski (219338951)and Ohad Hasson(217803477)
  * @version 26/11/2025 17:00
  */
 import java.util.*;
@@ -15,13 +15,12 @@ public class Encryption
         System.out.println("Enter 1 for Encrypt | 2 for Decrypt");
 
         int programNum = reader.nextInt();
-        if (Encryption.checkNum(programNum)) {
+        if (checkNum(programNum)) {
             return;
         }
-        reader.nextLine(); // לניקוי השורה
+        reader.nextLine();
         System.out.println("Enter up to 3 words sentence");
         String sentence = reader.nextLine();
-        
         int words = wordsNum(sentence);
 
         if (words > 3) {
@@ -61,19 +60,22 @@ public class Encryption
         String word2 = "";
         String word3 = "";
         String result = "";
-        if (words == 2) {
+        if (words == 1)
+            result = sentence;
+        else if (words == 2){
             word1 = sentence.substring(0, sentence.indexOf(" "));
-            word2 = sentence.substring(sentence.indexOf(" ") + 1, sentence.lastIndexOf(" "));
+            word2 = sentence.substring(sentence.indexOf(" ")+1);
             result = word2 + ' ' + word1;
         } else {
             word1 = sentence.substring(0, sentence.indexOf(" "));
-            word2 = sentence.substring(sentence.indexOf(" ") + 1, sentence.lastIndexOf(" "));
-            word3 = sentence.substring(sentence.lastIndexOf(" ") + 1);
+            word2 = sentence.substring(sentence.indexOf(" ")+1, sentence.lastIndexOf(" "));
+            word3 = sentence.substring(sentence.lastIndexOf(" ")+1);
             result = word3 + ' ' + word1 + ' ' + word2;
         }
-       
-        char c = sentence.charAt(sentence.length() - words);
-        result = c + sentence.substring(0, sentence.length() - words);
+        
+        String c = result.substring(result.length() - words);
+        result = result.substring(0, result.length() - words);
+        result = c + result;
         result = result.replace('a', '@');
         result = result.replace('e', '#');
         result = result.replace('i', '1');
@@ -90,9 +92,9 @@ public class Encryption
         result = result.replace('0', 'o');
         result = result.replace('&', 'u');
 
-        char c = result.charAt(0);
-        result = result.substring(1) + c;
-
+        String c = result.substring(0, words);
+        result = result.substring(words) + c;
+        
         String word1 = "";
         String word2 = "";
         String word3 = "";
