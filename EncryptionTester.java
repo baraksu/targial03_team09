bd9cadc249a829da88a4b2d843521ab95de3190e
i5am5cool <ohad.hasson2010@gmail.com>
2025-11-28 11:14:50 +0200

    Update EncryptionTester.java

--- EncryptionTester.java
+++ EncryptionTester.java
@@ -13,9 +13,17 @@ public class EncryptionTester
     @Test
     public void testAdd(){
         
-        int result = Encryption.wordsNum("word1 word2");
+        int result = encryption.wordsNum("word1 word2");
         assertEquals(2,result);
-        
+
+        boolean bool = encription.chekNum(3);
+        assertEquals(bool, true);
+
+        String str = encription.encriptipon("very cool", 2);
+        assertEquals(str, "ryc00l v#");
+
+        str = encrption.decription("ryc00l v#", 2);
+        assertEquls("very cool");
     }
     
 }
